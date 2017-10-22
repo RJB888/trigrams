909d52cf5a13b2589254e1e94c4229a435c2374d
Darren-Haynes <darrenhaynes@zoho.com>
2017-10-22 14:09:42 -0700

    added test to test that read_book_from_file() function returns a string

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -11,6 +11,12 @@ def get_book():
 book = get_book()
 
 
+def test_read_book_from_file():
+    """Test that book is converted to and returned as a string"""
+    from trigrams import read_book_from_file
+    assert type(read_book_from_file()) == str
+
+
 # @pytest.mark.parametrize('book, result', BOOKS_TABLE)
 def test_words_are_only_alpha():
     """Test all non-alpha characters have been removed from text"""

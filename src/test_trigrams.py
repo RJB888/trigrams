33d2f698305c344d1ed484030026c7c5bbd4ec9a
Darren-Haynes <darrenhaynes@zoho.com>
2017-10-22 13:18:12 -0700

    added test for convert_book_to_list_of_words() function

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -1,4 +1,21 @@
 """Test that the trigrams algorithm produces proper output."""
-
 import pytest
+
+
+def get_book():
+    with open('sherlock.txt', 'r') as book:
+        book = book.read()
+    return book
+
+
+book = get_book()
+
+
+# @pytest.mark.parametrize('book, result', BOOKS_TABLE)
+def test_words_are_only_alpha():
+    """Test all non-alpha characters have been removed from text"""
+    from trigrams import convert_book_to_list_of_words
+    assert  ''.join(convert_book_to_list_of_words(book)).isalpha()
+
+
 

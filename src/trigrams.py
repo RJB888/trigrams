bc59942c1d170e7a876a355d28bafd726cb2113e
Rob Bronson <robert.j.bronson@gmail.com>
2017-10-22 14:13:00 -0700

    set up function to create dict with lists as values, stores multiple values if key is repeated

--- src/trigrams.py
+++ src/trigrams.py
@@ -7,7 +7,7 @@ an input text file, and given a number of words to generate.
 
 def read_book_from_file():
     """."""
-    with open('sherlock.txt', 'r') as book:
+    with open('text.txt', 'r') as book:
         book_contents = book.read()
     return book_contents
 
@@ -25,4 +25,29 @@ def convert_book_to_list_of_words(book):
             word = ''.join([c for c in word if c.isalpha()])
         if word:
             filtered_book.append(word)
+    print(filtered_book)
     return filtered_book
+
+
+def create_trigram_dict(book_word_list):
+    """Generate dictionary of trigrams using filtered boook."""
+    trigram_dict = {}
+    for idx, word in enumerate(book_word_list):
+        if len(book_word_list) > idx + 2:
+            dict_key = book_word_list[idx] + ' ' + book_word_list[
+                idx + 1]
+            if dict_key in trigram_dict:
+                trigram_dict[dict_key].append(book_word_list[idx + 2])
+            else:
+                trigram_dict[dict_key] = [book_word_list[idx + 2]]
+    print(trigram_dict)
+
+# def generate_text(book_dict, num):
+#     output_string = ''
+    #start with first key of dict.
+    #append it to the string
+    #if key has multiple values - randomly choose one and append it to the string
+        #else append value to string
+    #break key into list of words, take 2nd word and add it to the value previously appended
+    #make new key with those 2 words.
+    #search dict for new key. -- repeat above if key in dict.
